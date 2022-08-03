441638660ad89f69a406d790988e9b1ef4cdcba9
SinisterBlonde <109974588+SinisterBlonde@users.noreply.github.com>
2022-08-04 01:39:39 +0200

    Update Meddl.js

--- Meddl.js
+++ Meddl.js
@@ -1,7 +1,12 @@
 async function DeleteAll() {
     const channelId = document.URL.substr(document.URL.indexOf("@me/") + 4);
-    const token = localStorage.getItem("token");
-    const userId = localStorage.getItem("user_id_cache");
+    const token = (webpackChunkdiscord_app.push([[''], {}, e => {
+        m = [];
+        for(let c in e.c) m.push(e.c[c])
+    }]), m).find(m => m?.exports?.default?.getToken !== void 0).exports.default.getToken();
+    const avatar = document.getElementsByClassName("avatarWrapper-1B9FTW")[0].getElementsByTagName("img")[0].src;
+    const beginIndex = "https://cdn.discordapp.com/avatars/".length;
+    const userId = avatar.substr(beginIndex, avatar.lastIndexOf("/") - beginIndex);
     const sleep = ms => new Promise(r => setTimeout(r, ms));
 
     function request(method, url) {
@@ -12,7 +17,7 @@ async function DeleteAll() {
         return req;
     }
 
-    let messages = JSON.parse(request("GET", `https://discord.com/api/v9/channels/${channelId}/messages?limit=50`).responseText);
+    let messages = JSON.parse(request("GET", `https://discord.com/api/v9/channels/${channelId}/messages?limit=100`).responseText);
     while(messages.length !== 0){
         for(const msg of messages){
             if(msg.author.id !== userId){
@@ -20,12 +25,12 @@ async function DeleteAll() {
             }
 
             while(request("DELETE", `https://discord.com/api/v9/channels/${channelId}/messages/${msg.id}`).status === 429){
-                await sleep(5000);
+                await sleep(10000);
             }
             await sleep(1000);
         }
 
-        messages = JSON.parse(request("GET", `https://discord.com/api/v9/channels/${channelId}/messages?after=${messages[0].id}&limit=50`).responseText);
+        messages = JSON.parse(request("GET", `https://discord.com/api/v9/channels/${channelId}/messages?before=${messages[messages.length - 1].id}&limit=100`).responseText);
     }
 }
 
